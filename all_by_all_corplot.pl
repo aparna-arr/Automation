23d2efbccf72b45d58086f136ebe3ce51f8998fe
Aparna Rajiv Rajpurkar <arrajpur@crick.cse.ucdavis.edu>
2015-11-18 10:15:07 -0800

    Adding some figure making scripts (panelcor)

---
+++ all_by_all_corplot.pl
@@ -0,0 +1,73 @@
+#!/usr/bin/env perl
+use warnings;
+use strict;
+
+my ($dir) = @ARGV;
+die "usage: $0 <dir>\n" unless @ARGV == 1;
+
+opendir(DIR, $dir);
+my @files = readdir(DIR);
+closedir(DIR);
+
+my %hash;
+foreach my $file (@files)
+{
+	next if (-d $file || $file =~ /rep[12]/ || $file !~ /\.mapwig$/);
+
+        my ($wig, $bed) = $file =~ /(.+?(?:_GSE\d+)*)_(.+?)(?:_combined_reps|\.)/;
+	
+	print "wig [$wig] bed [$bed]\n";
+
+	$hash{$bed}{$wig} = $file;
+}
+
+open (OUT, ">", "Master.sh");
+foreach my $beds (keys %hash)
+{
+	print OUT "R --no-save < $beds.R\n";
+	open (R, ">", "$beds.R");
+
+	print R "library(reshape2)
+library(ggplot2)
+source(\"../../panels.R\")
+";
+
+	my @colnames;
+	foreach my $wigs (sort keys %{$hash{$beds}})
+	{
+		push (@colnames, $wigs);
+		print R "
+$wigs<-read.delim(\"$hash{$beds}{$wigs}\", header=F)
+$wigs<-$wigs\$V4
+#quartile<-quantile($wigs, na.rm=TRUE)
+
+# remove outliers
+#$wigs\[$wigs < quartile[2]] <- NA
+#$wigs\[$wigs > quartile[4]] <- NA
+
+
+$wigs.log<-$wigs
+$wigs.log[$wigs.log == 0] <- NA
+$wigs.log <- log10($wigs.log)
+
+
+";
+	}
+
+	print R "data<-as.data.frame(cbind(" . join(".log,", @colnames) . "))\n";
+#	print R "data<-as.data.frame(cbind(" . join(",", @colnames) . "))\n";
+	print R "colnames(data)<-c(" . (join ',', map qq("$_"), @colnames) . ")\n";
+#	print R "data.corr<-round(cor(data, method=\"p\", use=\"na.or.complete\"), 2)\n";
+#	print R "data.corr.m<-melt(data.corr)\n";
+	print R "png(\"$beds\_corr.png\", width=1200, height=1000)\n";
+#	print R "
+#ggplot(data.corr.m, aes(x=Var1, y=Var2, fill=value)) + geom_tile() + geom_text(aes(Var1, Var2, label=value)) + scale_fill_gradient2(low=\"darkblue\", mid=\"white\", high=\"darkred\", limits=c(-1,1), midpoint=0) + ggtitle(\"$beds Corrplot\") + xlab(\"\") + ylab(\"\")
+#";
+
+	print R "panelcor(data, \"$beds\")\n";
+	print R "dev.off()\n";
+
+	close R;
+}
+close OUT;
+`bash Master.sh`;
